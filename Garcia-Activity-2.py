973d622fd104db8887ba3d4966b4cb6f707e4c99
NetAcad DEVASC <changeme@example.com>
2021-09-26 12:24:01 +0000

    second part

--- Garcia-Activity-2.py
+++ Garcia-Activity-2.py
@@ -6,3 +6,9 @@ Semis = float(input("Enter your semis score here: "))
 Finals = float(input("Enter your finals score here: "))
 
 avg = (Prelims+Midterms+Semis+Finals)/4
+
+avg=("Your average is {}!".format(avg))
+if avg>=75:
+    print("You passed!")
+elif avg<75:
+    print("You have failed.")
